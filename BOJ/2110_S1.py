6d70f78150539cac574f0c3b68bc30c071c18a42
이정준 <junnnyyy@kakao.com>
2021-07-19 23:52:28 +0900

    0719

--- BOJ/2110_S1.py
+++ BOJ/2110_S1.py
@@ -1,11 +1,28 @@
-def bin_search(s, e):
-    while s <= e:
-        m = (s + e) // 2
+def bin_search(l, r):
+    ans = 0
+    while l <= r:
+        gap = (l + r) // 2
+        cnt, start = 0, s
+        for x in X:
+            if x - start >= gap: # 기준간격보다 멀면
+                start = x
+                cnt += 1 # 공유기 설치
+        if cnt >= c: # 더 넓게 설치할 수 있겠다 싶으면.
+            ans = gap
+            l = gap + 1
+        else:
+            r = gap -1
+    return ans
 
 
 N, C = map(int, input().split())
-X = []
+X, s = [], 0
+s = 0
 for _ in range(N):
-    X.append(int(input()))
+    if not _:
+        s = int(input())
+    else:
+        X.append(int(input()))
+r, c, l = X[-1], C - 1, s # l은 이진 탐색을 위함, s는 시작 기준점을 위함
 X.sort()
-print(X)
+print(bin_search(l, r))
