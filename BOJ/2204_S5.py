ca5b4dc13f81dadf656a57be92b8c69723710b6e
이정준 <junnnyyy@kakao.com>
2021-08-30 22:33:57 +0900

    5 3 1 2 8 4 9

---
+++ BOJ/2204_S5.py
@@ -0,0 +1,13 @@
+ans, low_ans = '', ''
+n = int(input())
+while n:
+    lst, low_lst = [], []
+    for _ in range(n):
+        tmp = input()
+        low_tmp = tmp.lower()
+        lst.append(tmp)
+        low_lst.append((low_tmp, _))
+    low_lst.sort()
+    print(lst[low_lst[0][1]])
+    n = int(input())
+
