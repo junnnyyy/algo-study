b4de23918e84781ac3943bf1a914c416747ff50a
이정준 <junnnyyy@kakao.com>
2021-10-14 19:59:58 +0900

    algorithm: JungOL 1169 - permutations, combination,

--- BOJ/11559_G5.py
+++ BOJ/11559_G5.py
@@ -1,10 +1,15 @@
+# 상하좌우
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
+
+# 터트릴 뿌요 정해주기
 def bfs(r, c, color):
-    global tmp_ans, is_bomb
+    global is_bomb
     q = [(r, c)]
+    # 터질 뿌요의 후보군을 담을 리스트
     candi = []
+    # 캔디체크 해준 자리인지 확인 해줄 리스트
     checked[r][c] = 1
     while q:
         r, c = q.pop(0)
@@ -15,33 +20,40 @@ def bfs(r, c, color):
                 q.append((nr, nc))
                 checked[nr][nc] = 1
     if len(candi) >= 4:
+        # 뿌요가 4개넘게 모여서 터트렸음을 체크
         is_bomb = True
-        print(candi)
+        # print(candi)
         for i in range(len(candi)):
             xr, xc = candi[i]
             lst[xr][xc] = 0
 
+
+# 뿌요 터트리기
 def rm_zero(lst):
     for i in range(6):
         y = len(lst[i])
-        for j in range(y-1, -1, -1):
+        for j in range(y - 1, -1, -1):
             if lst[i][j] == 0:
                 lst[i].pop(j)
     return
 
+
+# 인덱스 에러 방지를 위한 '.'을 채워주는 함수
 def padding(lst):
     for i in range(6):
         y = len(lst[i])
-        val = 12-y
+        val = 12 - y
         lst[i] += ['.'] * val
+
 
 lst = [[0] * 12 for _ in range(6)]
 for i in range(12):
     tmp = list(input())
     for j in range(len(tmp)):
-        lst[j][11-i] = tmp[j]
+        lst[j][11 - i] = tmp[j]
 
 ans = 0
+# 현재 반복에서 터진적 있는지 체크하기 위한 boolean 값
 is_bomb = True
 while True:
     checked = [[0] * 12 for _ in range(6)]
@@ -51,15 +63,15 @@ while True:
         y = len(lst[i])
         for j in range(y):
             if lst[i][j] != '.':
-                print(i, j, lst[i][j])
+                # print(i, j, lst[i][j])
                 bfs(i, j, lst[i][j])
     rm_zero(lst)
     padding(lst)
+    # 터졌으면 또 터질지 해보기
     if is_bomb:
-        print(ans)
         ans += 1
     else:
         break
-    for _ in range(6):
-        print(lst[_])
+    # for _ in range(6):
+    #    print(lst[_])
 print(ans)
